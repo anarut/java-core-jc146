ab74b0d8e63f2e5d56c8adf41092af3e01e66979
Andrey Narut <anarut@objectstyle.com>
2024-03-28 18:49:53 +0300

    lesson 6

--- src/lesson06/Main3.java
+++ src/lesson06/Main3.java
@@ -1,15 +1,15 @@
 package lesson06;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main3 {
 
     public static void main(String[] args) {
-        int[][] b = new int[5][];
         int[][] c =
                 {
                         {1, 2, 3},
-                        {1, 2, 3, 5,6,7},
+                        {1, 2, 3, 5, 6, 7},
                         {0}
                 };
 
@@ -50,5 +50,46 @@ public class Main3 {
 
         int[] ints1 = a[0];
         int i = a[0][3];
+
+
+        int[][] b = new int[5][];
+        b[0] = new int[10];
+        b[1] = new int[]{1, 2, 3};
+        b[2] = new int[]{1, 2, 3};
+        b[3] = new int[]{1, 2, 3};
+        b[4] = new int[]{1, 2, 3};
+
+        int[][][] d = new int[2][][];
+        d[0] = a;
+        d[1] = b;
+
+        int i1 = d[0][0][0];
+
+
+        int n = 1000;
+
+        // int   4 byte?
+        int[] ints2 = new int[n]; // 4 000 byte ~  4kb
+        int[][] ints3 = new int[n][n]; // 4000kb ~ 4mb
+        int[][][] ints4 = new int[n][n][n]; // 4000 mb ~ 4gb
+        int[][][][] ints5 = new int[n][n][n][n]; // 4000 gb ~ 4tb
+        byte[][][][] ints6 = new byte[n][n][n][n]; // 1tb
+
+
+        Arrays.asList(ints2);
+
+        Arrays.fill(ints2, 555);
+
+//        for (int k = 0, len = ints2.length; k < len; k++)
+//            ints2[k] = 555;
+
+//        int i2 = ints2[2];
+//        int i3 = ints2[3];
+
+        ints2[3] = ints2[2];
+
+        int temp = ints2[3];
+        ints2[3] = ints2[2];
+        ints2[2] = temp;
     }
 }
